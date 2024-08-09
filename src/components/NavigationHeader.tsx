bcb31a81036d9566756eba33bc6d912b8f286da5
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-09 13:05:31 +0100

    Add the images to the page

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -1,14 +1,24 @@
+import facebook from '../images/001-facebook.svg'
+import twitter from '../images/003-twitter.svg'
+import instagram from '../images/004-instagram.png'
+
+
 const NavigationHeader = () => {
     return (
       <div className="h-32 top-40 left-80 w-full flex flex-row items-center justify-between px-8">
         <h2 className="font-lobster font-normal text-2xl">Foodieland</h2>
-        <ul className="flex flex-row space-x-8">
+        <ul className="flex flex-row space-x-8 font-semibold">
           <li>Home</li>
           <li>Recipes</li>
           <li>Blog</li>
           <li>Contact</li>
           <li>About Us</li>
         </ul>
+        <div className='flex flex-row w-24 justify-between'>
+            <img src={facebook} alt="facebook logo" />
+            <img src={twitter} alt="facebook logo" />
+            <img src={instagram} alt="facebook logo" />
+        </div>
       </div>
     );
   };
