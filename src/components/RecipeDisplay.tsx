b596a20546d70d45b1a1885c2bccd8e0c78dae06
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-09 15:13:53 +0100

    Text for Recipe Display and add images for ImageGrid

--- src/components/RecipeDisplay.tsx
+++ src/components/RecipeDisplay.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const RecipeDisplay = () => {
   return (
-    <div className='text-center'>
-    <h1>Simple and Tasty Recipes </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum error, alias amet debitis atque vero nam ducimus voluptatum voluptatem dolorum.</p>
-    
+    <div className="text-center">
+      <h1 className="text-5xl font-semibold">Simple and Tasty Recipes </h1>
+      <p className="text-base">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p className="text-base">
+        Laborum error, alias amet debitis atque vero
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeDisplay
+export default RecipeDisplay;
