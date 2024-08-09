0d007b5c8f7462b8779a5ccc427f510113c6a5b8
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-09 13:25:02 +0100

    Add the other 2 rectangles to complete the recipe page

--- src/components/FeaturedRecipe.tsx
+++ src/components/FeaturedRecipe.tsx
@@ -1,8 +1,14 @@
-import RecipePage from '../images/Featured Recipe.svg'
+import RecipePage from "../images/Featured Recipe.svg";
+import RightRectangle from "../images/Rectangle 6.svg";
+import LeftRectangle from "../images/Rectangle 5.svg";
 const FeaturedRecipe = () => {
   return (
-    <div className='w-full justify-center h-fit flex'><img src={RecipePage} alt="Recipe Image" /></div>
-  )
-}
+    <div className="w-full justify-center h-fit flex">
+      <img className="mr-auto" src={LeftRectangle} alt="The left side of the Recipe Page" />
+      <img src={RecipePage} alt="Recipe Image" />
+      <img className="ml-auto" src={RightRectangle} alt="The right side of the Recipe Page" />
+    </div>
+  );
+};
 
-export default FeaturedRecipe
+export default FeaturedRecipe;
