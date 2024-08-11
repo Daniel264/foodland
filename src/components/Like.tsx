c90e8ac0f84c7e5cd8c1399a2a8c927d5ab59ad4
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-11 03:09:37 +0100

    Create a reusable like component

---
+++ src/components/Like.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+interface Props {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: Props) => {
+  const [status, setStatus] = useState(true);
+
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  };
+  if (status) return <AiFillHeart color="#ff6b81" size={20} onClick={toggle} />;
+  return <AiOutlineHeart size={20} onClick={toggle} />;
+};
+
+export default Like;
