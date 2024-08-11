1793f8668ef7723953303f4d051f48993698c3ed
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-11 14:50:49 +0100

    Add checked and unchecked to directions page

--- src/components/DummyText.tsx
+++ src/components/DummyText.tsx
@@ -1,22 +1,38 @@
+import React from "react";
+import checked from "../images/Checked.svg";
+import unchecked from "../images/Unchecked.svg";
 
 interface Props {
-  id: number
+  id: number;
+  isChecked: boolean;
+  toggleChecked: () => void;
 }
-const DummyText = ({id}: Props) => {
+
+const DummyText = ({ id, isChecked, toggleChecked }: Props) => {
   return (
-    <>
-      <h2 className="font-semibold text-3xl pb-4">{id}. Lorem ipsum dolor sit amet.</h2>
-      <p>
+    <div className="flex items-center">
+      <div>
+        <h2 className="font-semibold text-3xl pb-4 flex">
+          {" "}
+          <img
+            className="cursor-pointer mr-4"
+            src={isChecked ? checked : unchecked}
+            alt={isChecked ? "Checked" : "Unchecked"}
+            onClick={toggleChecked}
+          />
+          {id}. Lorem ipsum dolor sit amet.
+        </h2>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed qui cum,
-          maiores, corrupti animi quod dolores illo ipsa, officiis iste molestiae
-          enim! Neque deleniti consequuntur vitae unde! Itaque fuga cupiditate rerum
-          assumenda ea dignissimos nostrum dolorum quod tempora obcaecati unde quo
-          voluptatum alias id officiis nemo officia, consequuntur iure?
-          Reprehenderit quam atque expedita voluptatibus et pariatur ullam excepturi
-          veniam ea.
-          lorem30
-      </p>
-    </>
+          maiores, corrupti animi quod dolores illo ipsa, officiis iste
+          molestiae enim! Neque deleniti consequuntur vitae unde! Itaque fuga
+          cupiditate rerum assumenda ea dignissimos nostrum dolorum quod tempora
+          obcaecati unde quo voluptatum alias id officiis nemo officia,
+          consequuntur iure? Reprehenderit quam atque expedita voluptatibus et
+          pariatur ullam excepturi veniam ea. lorem30
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/innerComponent/Directions.tsx
+++ src/innerComponent/Directions.tsx
@@ -1,23 +1,46 @@
-import DummyImage from '../images/DirectionImage.svg'
-import DummyText from "../components/DummyText";
-
+import { useState } from 'react';
+import DummyImage from '../images/DirectionImage.svg';
+import DummyText from '../components/DummyText';
 
 const Directions = () => {
+  // Initial state: all items are unchecked
+  const [checkedItems, setCheckedItems] = useState({
+    1: false,
+    2: false,
+    3: false,
+  });
+
+  // Function to toggle checked state
+  const toggleChecked = (id: number) => {
+    setCheckedItems((prevCheckedItems) => ({
+      ...prevCheckedItems,
+      [id]: !prevCheckedItems[id],
+    }));
+  };
+
   return (
-    <>
-      <div className="w-8/12 p-14">
-        <h1 className="font-semibold text-4xl pb-3">Directions</h1>
-        <DummyText id={1} />
-        <img className='w-full py-10' src={DummyImage} alt="" />
-        <hr className='border-gray-200 pb-10'/>
-        <DummyText id={2}/>
-        <hr className='my-10' />  
-        <DummyText id={3}/>
-        <hr className='my-10' />  
-
-
-      </div>
-    </>
+    <div className="w-8/12 p-14">
+      <h1 className="font-semibold text-4xl pb-3">Directions</h1>
+      <DummyText
+        id={1}
+        isChecked={checkedItems[1]}
+        toggleChecked={() => toggleChecked(1)}
+      />
+      <img className="w-full py-10" src={DummyImage} alt="" />
+      <hr className="border-gray-200 pb-10" />
+      <DummyText
+        id={2}
+        isChecked={checkedItems[2]}
+        toggleChecked={() => toggleChecked(2)}
+      />
+      <hr className="my-10" />
+      <DummyText
+        id={3}
+        isChecked={checkedItems[3]}
+        toggleChecked={() => toggleChecked(3)}
+      />
+      <hr className="my-10" />
+    </div>
   );
 };
 
